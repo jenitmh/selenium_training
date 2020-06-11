a5d0cf61088e31c2388668e83b7be52bb190f3a2
Evgeniya <yudina.evgeniya@itmh.ru>
2020-06-11 12:33:28 +0500

    Добавлены Assert.True и комментарии к методам и строкам инструкций

--- selenium_training/MenuItems.cs
+++ selenium_training/MenuItems.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 
-namespace selenium_training
+namespace SeleniumTraining
 {
     [TestFixture]
     public class MenuItems
@@ -20,7 +20,7 @@ namespace selenium_training
         public void Start()
         {
             driver = new ChromeDriver();
-            baseUrl = "http://localhost/litecart";
+            baseUrl = "http://localhost/litecart/";
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
         }
@@ -30,260 +30,54 @@ namespace selenium_training
         {
             Login();
             FindMenuItem();
-            
-            //ClickMenuItem();
-
-            /*Appearance();
-            HeaderAvailable();
-
-            Catalog();
-            HeaderAvailable();
-
-            Countries();
-            HeaderAvailable();
-
-            Currencies();
-            HeaderAvailable();
-
-            Customers();
-            HeaderAvailable();
-
-            Geo_zones();
-            HeaderAvailable();
-
-            Languages();
-            HeaderAvailable();
-            
-            Modules();
-            HeaderAvailable();
-
-            Orders();
-            HeaderAvailable();
-
-            Pages();
-            HeaderAvailable();
-
-            Reports();
-            HeaderAvailable();
-
-            Slides();
-            HeaderAvailable();
-
-            Settings();
-            HeaderAvailable();
-            
-            Tax();
-            HeaderAvailable();
-
-            Translations();
-            HeaderAvailable();
-
-            Users();
-            HeaderAvailable();
-
-            Vqmods();
-            HeaderAvailable();*/
-        }
-
-        private void ClickMenuItem()
-        {
-            var MenuItemsList = driver.FindElements(By.CssSelector("li.app"));
-
-            for (int i = 0; i < MenuItemsList.Count; i++)
-            {
-                MenuItemsList[i].Click();
-            }
         }
 
         private void Login()
         {
-            driver.Url = baseUrl + "/admin/login.php";
+            driver.Url = baseUrl + "admin/login.php";
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin" + Keys.Enter);
         }
 
-        private void Appearance()
-        {
-            string[] section =
-            {
-                "[data-code=appearance]",
-                "[data-code=template]",
-                "[data-code=logotype]"
-            };
-
-            foreach (string i in section)
-            {
-                driver.FindElement(By.CssSelector(i)).Click();
-            }
-        }
-
-        private void Catalog()
-        {
-            string[] section =
-            {
-                "[data-code=catalog]",
-                "[data-code=attribute_groups]",
-                "[data-code=option_groups]",
-                "[data-code=manufacturers]",
-                "[data-code=suppliers]",
-                "[data-code=delivery_statuses]",
-                "[data-code=sold_out_statuses]",
-                "[data-code=quantity_units]",
-                "[data-code=csv]"
-            };
-
-            foreach (string i in section)
-            {
-                driver.FindElement(By.CssSelector(i)).Click();
-            }
-        }
-
+        /// <summary>
+        /// Прокликивает все пункты и подпункты меню
+        /// </summary>
         private void FindMenuItem()
         {
-            int sizeMenu = driver.FindElements(By.CssSelector("li.app")).Count;
-            IWebElement[] sectionsMenu = new IWebElement[sizeMenu];
+            // Пемеренная с количеством пунктов меню на странице
+            int quantityMenu = driver.FindElements(By.CssSelector("li.app")).Count;
 
-            for (int m = 0; m < sectionsMenu.Length; m++)
+            // Перебор всех пунктов меню
+            for (int m = 1; m <= quantityMenu; m++)
             {
-                sectionsMenu[m] = driver.FindElement(By.CssSelector(".app:nth-child(" + (m + 1) + ")"));
-                sectionsMenu[m].Click();
+                // Клик по пункту меню
+                driver.FindElement(By.CssSelector(".app:nth-child(" + m + ")")).Click();
+                HeaderAvailable();
 
-                int sizeSub = driver.FindElements(By.CssSelector("li.doc")).Count;
-                IWebElement[] sectionsSub = new IWebElement[sizeSub];
+                // Пемеренная с количеством пунктов подменю
+                int quantitySub = driver.FindElements(By.CssSelector("li.doc")).Count;
 
+                // Определение наличия пунктов подменю
                 if (IsElementPresent(By.CssSelector("li.doc")))
                 {
-                    for (int s = 0; s < sectionsSub.Length; s++)
+                    // Перебор всех пунктов подменю
+                    for (int s = 1; s <= quantitySub; s++)
                     {
-                        sectionsSub[s] = driver.FindElement(By.CssSelector(".doc:nth-child(" + (s + 1) + ")"));
-                        sectionsSub[s].Click();
+                        // Клик по пункту меню
+                        driver.FindElement(By.CssSelector(".doc:nth-child(" + s + ")")).Click();
+                        HeaderAvailable();
                     }
                 }
             }
+            Assert.True(quantityMenu > 0, "Нет пунктов меню");
         }
 
-        private void Countries()
-        {
-            driver.FindElement(By.CssSelector("[data-code=countries]")).Click();
-        }
-
-        private void Currencies()
-        {
-            driver.FindElement(By.CssSelector("[data-code=currencies]")).Click();
-        }
-
-        private void Customers()
-        {
-            string[] section =
-            {
-                "[data-code=customers]",
-                "[data-code=csv]",
-                "[data-code=newsletter]"
-            };
-
-            foreach (string i in section)
-            {
-                driver.FindElement(By.CssSelector(i)).Click();
-            }
-        }
-
-        private void Geo_zones()
-        {
-            driver.FindElement(By.CssSelector("[data-code=geo_zones]")).Click();
-        }
-
-        private void Languages()
-        {
-            string[] section =
-            {
-                "[data-code=languages]",
-                "[data-code=storage_encoding]"
-            };
-
-            foreach ( string i in section)
-            {
-                driver.FindElement(By.CssSelector(i)).Click();
-            }
-        }
-
-        private void Modules()
-        {
-            driver.FindElement(By.CssSelector("[data-code=modules]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=customer]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=shipping]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=payment]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=order]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=order_total]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=jobs]")).Click();
-        }
-
-        private void Orders()
-        {
-            driver.FindElement(By.CssSelector("[data-code=orders]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=order_statuses]")).Click();
-        }
-
-        private void Pages()
-        {
-            driver.FindElement(By.CssSelector("[data-code=pages]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=csv]")).Click();
-        }
-
-        private void Reports()
-        {
-            driver.FindElement(By.CssSelector("[data-code=reports]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=monthly_sales]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=most_sold_products]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=most_shopping_customers]")).Click();
-        }
-
-        private void Slides()
-        {
-            driver.FindElement(By.CssSelector("[data-code=slides]")).Click();
-        }
-
-        private void Settings()
-        {
-            driver.FindElement(By.CssSelector("[data-code=settings]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=store_info]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=defaults]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=email]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=listings]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=legal]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=images]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=checkout]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=advanced]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=security]")).Click();
-        }
-
-        private void Tax()
-        {
-            driver.FindElement(By.CssSelector("[data-code=tax]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=tax_rates]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=tax_classes]")).Click();
-        }
-
-        private void Translations()
-        {
-            driver.FindElement(By.CssSelector("[data-code=translations]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=search]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=scan]")).Click();
-            driver.FindElement(By.CssSelector("[data-code=csv]")).Click();
-        }
-
-        private void Users()
-        {
-            driver.FindElement(By.CssSelector("[data-code=users]")).Click();
-        }
-
-        private void Vqmods()
-        {
-            driver.FindElement(By.CssSelector("[data-code=vqmods]")).Click();
-        }
-
+        /// <summary>
+        /// Проверяет наличие заголовка на странице
+        /// </summary>
         private void HeaderAvailable()
         {
-            IsElementPresent(By.CssSelector("div.panel-heading"));
+            Assert.True(IsElementPresent(By.CssSelector("div.panel-heading")), "У пункта меню нет заголовка"); // Проверка пункта меню на наличие заголовка
         }
 
         private bool IsElementPresent(By by)
--- selenium_training/ProductSticker.cs
+++ selenium_training/ProductSticker.cs
@@ -7,7 +7,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
 
-namespace selenium_training
+namespace SeleniumTraining
 {
     [TestFixture]
     public class ProductStikers
@@ -33,20 +33,29 @@ namespace selenium_training
 
         }
 
+        /// <summary>
+        /// Проверяет количество стикеров у товара
+        /// </summary>
         private void StickerChecker()
         {
-            var Products = driver.FindElements(By.CssSelector("article.product-column"));
-            int index = 0;
+            // Переменная списка со всеми карточками товара найденными на странице
+            var productList = driver.FindElements(By.CssSelector("article.product-column"));
+            int productIndex = 0;
 
-            for (int i = 0; i < Products.Count; i++)
+            // Перебор всех карточек товара
+            for (int i = 0; i < productList.Count; i++)
             {
-                var Sticker = Products[i].FindElements(By.CssSelector("div.sticker"));
+                // Переменная со списком всех стикеров товара
+                var Sticker = productList[i].FindElements(By.CssSelector("div.sticker"));
+
+                // Проверка всех товаров на наличие единственного стикера
                 if (Sticker.Count == 1)
                 {
-                  index++;
+                    productIndex++;
                 }
             }
-            Assert.True(index == Products.Count);
+            Assert.True(productList.Count > 0, "Нет товаров на странице");
+            Assert.True(productIndex == productList.Count, "У " + (productList.Count - productIndex) + " из " + productList.Count + " товаров нет стикера");
         }
 
         [TearDown]
