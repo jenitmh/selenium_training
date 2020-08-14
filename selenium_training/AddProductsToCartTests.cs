64a2052521ae973ac27f9ac44762c98e5f330623
Evgeniya <yudina.evgeniya@itmh.ru>
2020-08-14 18:43:07 +0500

    Исправления

--- selenium_training/AddProductsToCartTests.cs
+++ selenium_training/AddProductsToCartTests.cs
@@ -27,26 +27,24 @@ namespace SeleniumTraining
         [Test]
         public void AddProductsToCartTest()
         {
-            for (int i = 0; i < 4; i++)
+            AddProductsToCart(3);
+            GoToCart();
+            RemoveProducts();
+        }
+
+        /// <summary>
+        /// Добавляет указанное количество товаров в корзину.
+        /// </summary>
+        /// <param name="quantity"></param>
+        private void AddProductsToCart(int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
             {
                 OpenHomePage();
                 OpenProductPage(0);
-                SelectSize('m');
+                SelectSize("Small");
                 AddToCart();
             }
-
-            OpenHomePage();
-            OpenProductPage(1);
-            SelectSize('l');
-            AddToCart();
-
-            OpenHomePage();
-            OpenProductPage(4);
-            SelectSize('s');
-            AddToCart();
-
-            GoToCart();
-            RemoveProducts();
         }
 
         /// <summary>
@@ -79,32 +77,6 @@ namespace SeleniumTraining
         }
 
         /// <summary>
-        /// Удаляет один товар из корзины (в качестве аргумента указывается порядковый номер товара в корзине).
-        /// </summary>
-        /// <param name="index"></param>
-        private void RemoveProducts(int index)
-        {
-            // Условие, что текущая страница является страницей корзины
-            if (IsElementPresent(By.ClassName("box")) && driver.Url == "http://localhost/litecart/checkout")
-            {
-                // Переменная со списком товаров в корзине
-                var productList = driver.FindElements(By.ClassName("item"));
-                // Переменная с количеством товаров в корзине до удаления
-                int quantityProductsOld = driver.FindElements(By.ClassName("item")).Count;
-
-                // Удаление товара из корзины с порядковым номером переданным методу аргументом
-                productList[index].FindElement(By.Name("remove_cart_item")).Click(); // -- Добавить ожидание обновления таблицы после удаления товара
-
-                // Переменная с количеством товаров в корзине после удаления
-                int quantityProductsNew = driver.FindElements(By.ClassName("item")).Count;
-
-                // Проверка того, что в корзине стало на 1 товар меньше
-                quantityProductsOld -= 1;
-                Assert.AreEqual(quantityProductsNew, quantityProductsOld);
-            }
-        }
-
-        /// <summary>
         /// Переходит в корзину.
         /// </summary>
         private void GoToCart()
@@ -118,64 +90,29 @@ namespace SeleniumTraining
         [Obsolete]
         private void AddToCart()
         {
-            // Переменная с текстом элемента (значек с количеством товаров в корзине)
-            string badgeOld = driver.FindElement(By.ClassName("badge")).Text;
+            // Переменная с элементом значка количества товаров в корзине
+            IWebElement bage = driver.FindElement(By.ClassName("badge"));
+            // Переменная с количеством товаров в корзине, отображаемом на значке bage
+            int text = int.Parse(driver.FindElement(By.ClassName("badge")).Text);
 
-            // Условие, что количество товаров в корзине больше нуля
-            if (badgeOld != "")
-            {
-                // Переменная с количеством товаров в корзине до добавления
-                int badgeQuantityOld = int.Parse(badgeOld);
-
-                // Добавление товара в корзину
-                driver.FindElement(By.Name("add_cart_product")).Click();
-                // Ожидание добавления товара в корзину
-                Thread.Sleep(1000);
-                // Переменная с количеством товаров в корзине после добавления
-                int badgeQuantityNew = int.Parse(driver.FindElement(By.ClassName("badge")).Text);
-
-                // Проверка того, что в корзине стало на 1 товар больше
-                badgeQuantityOld += 1;
-                Assert.AreEqual(badgeQuantityOld, badgeQuantityNew);
-            }
-            // Условие, что в корзине нет товаров
-            if (badgeOld == "")
-            {
-                // Добавление товара в корзину
-                driver.FindElement(By.Name("add_cart_product")).Click();
-
-                // Ожидание добавления товара в корзину (появление значка на иконке корзины)
-                wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("badge")));
-                // Переменная с количеством товаров в корзине после добавления
-                string badgeNew = driver.FindElement(By.ClassName("badge")).Text;
-
-                // Проверка того, что в корзине стало на 1 товар больше
-                badgeOld += 1;
-                Assert.AreEqual(badgeNew, badgeOld);
-            }
+            // Добавление товара в корзину
+            driver.FindElement(By.Name("add_cart_product")).Click();
+            // Добавление в переменную к количеству товаров в корзине + 1
+            text += 1;
+            // Ожидание появления количества товара на значке bage + 1
+            wait.Until(ExpectedConditions.TextToBePresentInElement(bage, text.ToString()));
         }
 
         /// <summary>
         /// Выбирает размер продукта из выпадающего списка если у продукта есть такой параметр, если параметра нет - ничего не делает (аргументы: 's' - Small, 'm' - Medium, 'l' - Large).
         /// </summary>
         /// <param name="size"></param>
-        private void SelectSize(char size)
+        private void SelectSize(string size)
         {
             if (IsElementPresent(By.Name("options[Size]")))
             {
                 driver.FindElement(By.Name("options[Size]")).Click();
-                if (size == 's')
-                {
-                    new SelectElement(driver.FindElement(By.Name("options[Size]"))).SelectByValue("Small");
-                }
-                else if (size == 'm')
-                {
-                    new SelectElement(driver.FindElement(By.Name("options[Size]"))).SelectByValue("Medium");
-                }
-                else if (size == 'l')
-                {
-                    new SelectElement(driver.FindElement(By.Name("options[Size]"))).SelectByValue("Large");
-                }
+                new SelectElement(driver.FindElement(By.Name("options[Size]"))).SelectByValue(size);
             }
         }
 
